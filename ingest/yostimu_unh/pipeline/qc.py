7826af816df751f5be207bc688b53dddc2c7ebab
McVey <amcvey@nrel.gov>
2022-03-31 16:14:41 -0400

    Added a qc check from mhkit to demonstrate the possibility

--- ingest/yostimu_unh/pipeline/qc.py
+++ ingest/yostimu_unh/pipeline/qc.py
@@ -1,29 +1,37 @@
 import numpy as np
 from typing import Optional
 from tsdat import DSUtil, QualityChecker, QualityHandler
+from mhkit import qc
+import xarray as xr
 
 
-# TODO – Developer: Add your custom quality checker / quality handler. Rename these to
-# indicate more clearly what they do. You will need to update the pipeline config file
-# with the new class name.
-
-
-class CustomQualityChecker(QualityChecker):
+class StagnantDataChecker(QualityChecker):
     def run(self, variable_name: str) -> Optional[np.ndarray]:
 
         # False values in the results array mean the check passed, True values indicate
         # the check failed. Here we initialize the array to be full of False values as
         # an example. Note the shape of the results array must match the variable data.
-        results_array = np.full_like(
-            self.ds[variable_name].data,
-            fill_value=False,
-            dtype=bool,
-        )
+
+        # To utilize the qc functions from mhkit, first the data must be sent to a
+        # pandas data frame
+        data_to_pd = self.ds[variable_name].to_dataframe()
+
+        # Define expected lower bound (no upper bound is specified in this example)
+        expected_bound = [0.001, None]
+
+        # Define the moving window, in seconds
+        sample_rate = data_to_pd.index.values[0:2]
+        window = ((sample_rate[-1] - sample_rate[0]) / np.timedelta64(1, 's')) * 10
+
+        # Run the delta quality control test
+        results = qc.check_delta(data_to_pd, expected_bound, window)
+        results_array = ~results['mask']
+        results_array = results_array.to_numpy()
 
         return results_array
 
 
-class CustomQualityHandler(QualityHandler):
+class StagnantDataHandler(QualityHandler):
     def run(self, variable_name: str, results_array: np.ndarray):
 
         # Some QualityHandlers only want to run if at least one value failed the check.
